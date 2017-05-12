6278c5c39ea35f610e179c9ed0b364b7eb63bde6
astur4 <astur@MacBook-Pro-de-Roberto-2.local>
2017-05-12 20:21:56 +0200

    Estructura del proyecto

---
+++ config/global.php
@@ -0,0 +1,17 @@
+<?php
+
+    //IP del ordenador servidor de la base de datos
+    define("DB_HOST","localhost");
+
+    //Nombre de la base de datos
+    define("DB_NAME","dbsistema");
+
+    //Usuario de la base de datos
+    define("DB_USERNAME","root");
+
+    //Contraseña del usuario de la base de datos
+    define("DB_PASSWORD","");
+
+    define("DB_ENCODE","utf8");
+
+?>
